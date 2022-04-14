ff27192c25fd6ec0a68e5799e2c8c0f291752ea1
kuba jarosz <kuba.jarooosz@gmail.com>
2022-04-14 22:28:57 +0200

    19.2 resources file created

---
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.crud.tasks.repository;
+
+import com.crud.tasks.domain.Task;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends CrudRepository<Task, Long> {
+}
