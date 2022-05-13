f93dca9cecfd10505104850f7bd5a0c4071723cd
kuba jarosz <kuba.jarooosz@gmail.com>
2022-05-14 01:01:04 +0200

    make it better

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -14,7 +14,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("v1/trello")
+@RequestMapping("/v1/trello")
 @CrossOrigin("*")
 public class TrelloController {
 
