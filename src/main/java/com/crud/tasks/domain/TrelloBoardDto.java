d5aa7fb43fdb5d43d8e2d10dbf96eba2075e65e4
kuba jarosz <kuba.jarooosz@gmail.com>
2022-05-14 10:33:05 +0200

    make it better

--- src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
-@JsonIgnoreProperties(ignoreUnknown = true)
+@NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TrelloBoardDto {
     @JsonProperty("id")
     private String id;
